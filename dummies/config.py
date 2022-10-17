a3fc7af2da1253dd2fea8e7b9a0b566316af4fd6
Matthias van den Belt <mvandenbelt@bio-prodict.nl>
2022-10-17 22:30:41 +0200

    update to latest versions (i.e. adding csrf_key)

--- dummies/config.py
+++ dummies/config.py
@@ -2,32 +2,23 @@
 
 Author: Matthias van den Belt
 """
+from config_files.sensitive import csrf_key
+
 cagecat_version = '1.0'
 # When the version number is changed, /repo/maintenance/store_version_info.py should be run
 # also, the cache busting of the JavaScript file and CSS file in cagecat/templates/base.html
 # should be updated. (i.e. scripts.js?v=3.2 -> scripts.js?v=3.3)
 
-NCBI_ftp_base_url = 'ftp.ncbi.nlm.nih.gov'
-
-# MUST CHANGE:
-domain = '127.0.0.1:5364/'  # prefix of how you access your Docker instance
-
 # changeable
 send_mail = False
 period_to_keep_job_results = 31
-
-hmm_db_creation_conf = {
-    'sleeping_time': 60,
-    'cpus': '10',
-    'batch_size': '30'
-}
+domain = '127.0.0.1:5364/'
+NCBI_ftp_base_url = 'ftp.ncbi.nlm.nih.gov'
 
 thresholds = {
     'maximum_clusters_to_extract': 150,
     'maximum_gne_samples': 300,
     'max_clusters_to_plot': 75,
-    "prokaryotes_min_number_of_genomes": 50,
-    'fungi_min_number_of_genomes': 10
 }
 
 email_footer_msg = f'''Thank you for using our service. 
@@ -51,4 +42,5 @@ hmm_db_genome_downloads = '/hmm_db_downloads'
 init_config = {
     'SQLALCHEMY_DATABASE_URI': 'sqlite:////repo/cagecat/database.db',
     "SQLALCHEMY_TRACK_MODIFICATIONS": False,
+    'SECRET_KEY': csrf_key
 }
--- dummies/sensitive.py
+++ dummies/sensitive.py
@@ -1,7 +1,7 @@
 """Module to store sensitive information such as passwords
 
 Restart the container after editing this file
-docker stop <container_name> && docker start <container_name>
+docker restart <container_name>
 """
 
 # email configurations. These can be empty strings if you do not wish to send e-mails. The send_mail variable should be set to False (in config.py)
@@ -10,9 +10,11 @@ sender_email = ''  # address from where the e-mail should be sent
 port = None  # should be integer. Intentionally set as None
 
 account = ''  # to login  on SMTP server
-pwd = '' # corresponding password
+pwd = ''  # corresponding password
 
 # add jobs you do not wish to remove from your docker instance to the variable below
 persistent_jobs = (
 
                    )
+
+csrf_key = '!This_key_should_be_changed_manually_and_should_not_be_left_as_is!'
